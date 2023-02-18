24a142e424fdfb91c6543e437fb0c227208f4726
Babatunde Adeoti <babaolu98@gmail.com>
2023-02-18 13:17:42 -0800

    Completing hello
    _world task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Size of a char: %u byte(s)", sizeof(char));
+  printf("Size of a int: %u byte(s)", sizeof(int));
+  printf("Size of a long int: %u byte(s)", sizeof(long int));
+  printf("Size of a long long int: %u byte(s)", sizeof(long long int));
+  printf("Size of a float: %u byte(s)", sizeof(float));
+
+  return 0;
+}
