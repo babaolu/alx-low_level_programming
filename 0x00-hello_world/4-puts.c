56f161718ad5e7b3ccfd3f9cb9301026275c3aa0
Babatunde Adeoti <babaolu98@gmail.com>
2023-02-19 06:03:33 -0800

    Fixing 4-puts C file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/* Using puts to print out
+/**
+ * Using puts to print out
  * a line
  */
-
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-return (0);
+	return (0);
 }
