a16dc1fed542ff1f334cce1dc57a121e6e5729a2
Babatunde Adeoti <babaolu98@gmail.com>
2023-02-20 08:28:09 -0800

    Dealing with optional tasks

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = '0';
+	int b;
+
+	while (a < ('0' + 9))
+	{
+		b = a + 1;
+		while (b < ('0' + 10))
+		{
+			putchar(a);
+			putchar(b);
+
+		if (a == ('0' + 8))
+		{
+			continue;
+		}
+
+		putchar(',');
+		putchar(' ');
+		b++;
+		}
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
