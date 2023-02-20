4afe4ae8e1ff7294090238b9b4d0f9e2d835c3d6
Babatunde Adeoti <babaolu98@gmail.com>
2023-02-20 03:10:12 -0800

    Dealing with tasks 1 and 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alpha_a = 97;
+	int alpha_z = alpha_a + 25;
+	
+	while(alpha_a < (alpha_z +1))
+	{
+		putchar(alpha_a);
+		putchar('\n');
+		alpha_a++;
+	}
+
+	return (0);
+}
