f729e74d000e11c032ee75e7b8cf9a57de2d952a
Babatunde Adeoti <babaolu98@gmail.com>
2023-02-27 16:24:59 -0800

    Can random password work?

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Check code
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char result[16];
+
+	srand(time(NULL));
+
+	sprintf(result, "%d", rand());
+
+	execl("/root/alx-low_level_programming/0x05-pointers_arrays_\
+strings/101-crackme", "101-crackme", result, (char *) NULL);
+}
