eded9d3b83b7a029f48407fe249a1c0847158e60
Babatunde Adeoti <babaolu98@gmail.com>
2023-03-06 10:21:26 -0800

    Char to String

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,37 +1,28 @@
 #include "main.h"
 
 /**
- * _strspn - Gets length of prefix substring
+ * _strpbrk - Searches a string for any set of bytes
  * @s: String to parse
  * @accept: String containing desired characters
  *
  * Return: Number of bytes of initial segment
  */
-unsigned int _strspn(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
-	unsigned int num = 0;
-	int check = 0;
 	char *acc;
 
 	while (*s)
 	{
 		acc = accept;
-		check = 0;
 		while (*acc)
 		{
 			if (*s == *acc)
 			{
-				num++;
-				check++;
-				acc++;
-				break;
+				return (s);
 			}
 			acc++;
 		}
-		if (num && !check)
-			return (num);
-
 		s++;
 	}
-	return (num);
+	return (0);
 }
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring
+ * @haystack: String containing possible substring
+ * @needle: Possible substring to find
+ *
+ * Return: Location of substring when found,
+ * 0 if not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a;
+	char *pt;
+	char *hay;
+
+	while (*haystack)
+	{
+		if (*haystack == *needle)
+		{
+			pt = needle;
+			hay = haystack;
+			while (*pt)
+			{
+				if (*pt != *haystack)
+				{
+					a = 0;
+					haystack--;
+					break;
+				}
+				a = 1;
+				pt++;
+				haystack++;
+			}
+			if (a)
+				return (hay);
+			hay = 0;
+		}
+		haystack++;
+	}
+	return (0);
+}
