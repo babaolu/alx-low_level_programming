808f467c75e4fc31569c9f3cfc9c3274c022163d
Babatunde Adeoti <babaolu98@gmail.com>
2023-03-13 14:27:22 -0700

    Hiding in zeroes

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,19 +11,34 @@
  */
 int main(int argc, char *argv[])
 {
+	char *pname;
 	int count, add;
 	int res = 0;
 
 	for (count = 1; count < argc; count++)
 	{
 		add = atoi(argv[count]);
-		if (add || (*argv[count] == '0'))
+		if (add)
 			res += add;
+		else if (*argv[count] == '0')
+		{
+			pname = argv[count];
+			while (*pname)
+			{
+				if (*pname != '0')
+				{
+					printf("Error\n");
+					return (1);
+				}
+				pname++;
+			}
+		}
 		else
 		{
 			printf("Error\n");
 			return (1);
 		}
+
 	}
 	printf("%d\n", res);
 
