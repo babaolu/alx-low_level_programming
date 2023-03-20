9f602221a3dfabfa27852b329e2a86e73fda39aa
Babatunde Adeoti <babaolu98@gmail.com>
2023-03-20 11:08:00 -0700

    First pre-chip

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of source file
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf(__FILE__ "\n");
+
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+This project deals with the things the preprocessor can be capable of
