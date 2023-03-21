f3d43518388cc49d28d187fcf5f0be832094dfbc
Babatunde Adeoti <babaolu98@gmail.com>
2023-03-21 15:36:41 -0700

    Living wild and free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - Frees the dog_t struct
+ * @d: Dog to be freed
+ *
+ * Return: Void (Nothing)
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+		free(d);
+}
