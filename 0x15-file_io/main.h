c69226d29bfd8ae7e77297684c946c23728981db
Babatunde Adeoti <babaolu98@gmail.com>
2023-04-10 22:24:25 +0100

    Read and print to stdout

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+int _putchar(char);
+
+ssize_t read_textfile(const char *, size_t);
+
+#endif /* MAIN_H */
