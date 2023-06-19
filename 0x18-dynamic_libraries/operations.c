11744407f5695a4e1b0bd64087ecebd690397c67
Babatunde Adeoti <babaolu98@gmail.com>
2023-06-19 23:46:34 +0100

    Called C code from dynamic library inside python code

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,25 @@
+#include "operations.h"
+
+long int add(long int a, long int b)
+{
+	return (a + b);
+}
+long int mod(long int a, long int b)
+{
+	return (a % b);
+}
+
+long int sub(long int a, long int b)
+{
+	return (a - b);
+}
+
+long int mul(long int a, long int b)
+{
+	return (a * b);
+}
+
+long int div(long int a, long int b)
+{
+	return (a / b);
+}
---
+++ 0x18-dynamic_libraries/operations.h
@@ -0,0 +1,9 @@
+long int add(long int, long int);
+
+long int sub(long int, long int);
+
+long int mul(long int, long int);
+
+long int div(long int, long int);
+
+long int mod(long int, long int);
